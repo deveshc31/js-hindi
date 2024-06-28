99e270863553f440d66fcc23a5f422da3a02fb41
deveshc31 <104052300+deveshc31@users.noreply.github.com>
2024-06-28 08:53:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";                     //treat all js code as a newer version
+
+//alert(3+3)                      //we are using node js not browsers
+
+//console.log(3+3)
+//console.log("devesh")          //code is easily readable and there is no need of semicolun//
+
+//DATA TYPES
+let name="Devesh"       //string datatype
+let age=18              //number
+//let isLoggedIn=false    boolean
+//bigint
+//string
+//null
+//undefined
+//symbol
+
+//object(IMPORTANT)
+
+console.log(typeof null);
+console.log(typeof "devesh");
+console.log(typeof age);
+
