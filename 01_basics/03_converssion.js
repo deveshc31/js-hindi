a03e6a1516a72735d6995eda7d204ea755b351f6
deveshc31 <104052300+deveshc31@users.noreply.github.com>
2024-06-29 19:53:51 +0000

    comparission

---
+++ 01_basics/03_converssion.js
@@ -0,0 +1,79 @@
+/*let score="33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score) // valueInNumber se koi si bhi datatype ho vo number hi ayega
+console.log(typeof valueInNumber);
+console.log(valueInNumber) // value of this will be NaN
+
+// what can we convert into number and what we cannot
+//"33"=33
+//"33abc"=NaN
+//true=1;false=0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//what can be converted in boolean
+//1=true;0=false
+//" "=false(empty string)
+//"Devesh"=true
+
+let Devesh=33
+let stringNumber=String(Devesh)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//****************************************************************************************************
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2=" Devesh"
+let str3=(str1+str2)
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+//let gameCounter=100;
+//gameCounter++
+//console.log(gameCounter) // OUTPUT=101
+
+let gameCounter=100;
+++gameCounter
+console.log(gameCounter) // OUTPUT IS SAME AS ABOVE 101
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+let x=3
+const y=x++
+console.log(x);
+console.log(y);
+
+let x2=3
+const y2=++x2
+console.log(x2);
+console.log(y2);*/
+
+
+
+
+
